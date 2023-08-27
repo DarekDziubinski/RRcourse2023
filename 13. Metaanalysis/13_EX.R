add1f2ea1200f37e6681bb66cb4a4d27c7bd78ad
Darek Dziubinski <d.dziubinski@student.uw.edu.pl>
2023-08-27 17:14:04 +0200

    answer and comment to b) create a funnel plot (what do you see?) In my opinion, the funnel plot has an asymmetric shape, indicating a possible publication error or some other source of asymmetry. The higher heterogeneity (I^2 = 79.9%) causes them to be widely scattered, especially in the lower part of the plot. The points are stacked around values of -53.5 (for the fixed effects model / MD value) and -79.5 (for the random effects model / MD value). Overall, thedata suggest that there is a significant difference between study groups. (BOTH CHARTS / detailed description for the second chart)

--- 13. Metaanalysis/13_EX.R
+++ 13. Metaanalysis/13_EX.R
@@ -43,3 +43,12 @@ m.girls
 # Output / Conclusions
 # The model indicates that there is a significant effect in the included studies (p>value), both in the fixed-effects model and in the random-effects model. 
 # However, there is also significant heterogeneity (I^2) between studies, suggesting that not all studies report the same effect size.
+
+# II. create a funnel plot
+m.boys %>% funnel()
+m.girls %>% funnel()
+
+# In my opinion, the funnel plot has an asymmetric shape, indicating a possible publication error or some other source of asymmetry. 
+# The higher heterogeneity (I^2 = 79.9%) causes them to be widely scattered, especially in the lower part of the plot. 
+# The points are stacked around values of -53.5 (for the fixed effects model / MD value) and -79.5 (for the random effects model / MD value). 
+# Overall, thedata suggest that there is a significant difference between study groups. (BOTH CHARTS / detailed description for the second chart)
